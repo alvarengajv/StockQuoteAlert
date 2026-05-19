92f5d0431c06b4e85468f21188ee894efac55b01
João Vitor Alvarenga <alvarengajv@proton.me>
2026-05-18 22:22:23 -0300

    feat: adicao de interface de servicos de infra

---
+++ src/StockQuoteAlert.Domain/Interfaces/IMailKitService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockQuoteAlert.Domain.Interfaces
+{
+    public interface IMailKitService
+    {
+        Task SendEmailAsync(string subject, string body);
+    }
+}
---
+++ src/StockQuoteAlert.Domain/Interfaces/IYahooQuoteService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockQuoteAlert.Domain.Interfaces
+{
+    public interface IYahooQuoteService
+    {
+        Task<decimal> GetStockPriceAsync(string symbol);
+    }
+}
