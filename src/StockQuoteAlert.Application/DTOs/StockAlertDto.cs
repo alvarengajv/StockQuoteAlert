c8a87dd4c85baf0ed715680914a5ccb746071b1f
João Alvarenga <84135038+alvarengajv@users.noreply.github.com>
2026-05-19 14:46:16 -0300

    feat: Implementacao da camada application (#2)
    * feat: adicao de dto e interface dop servico
    
    * feat: adicao de servico de monitoramento
    
    * feat: adicao de validacoe e ajuste no servico

---
+++ src/StockQuoteAlert.Application/DTOs/StockAlertDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockQuoteAlert.Application.DTOs
+{
+    public class StockAlertDto
+    {
+        public required string Symbol { get; set; }
+        public required decimal SellPrice { get; set; }
+        public required decimal BuyPrice { get; set; }
+    }
+}
---
+++ src/StockQuoteAlert.Application/Validations/StockAlertDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StockQuoteAlert.Application.DTOs;
+
+namespace StockQuoteAlert.Application.Validations
+{
+    public static class StockAlertDtoValidator
+    {
+        public static void Validate(StockAlertDto dto)
+        {
+            if (string.IsNullOrWhiteSpace(dto.Symbol))
+                throw new ArgumentException("Símbolo não pode ser vazio.", nameof(dto.Symbol));
+
+            if (dto.SellPrice <= 0)
+                throw new ArgumentException("Preço de venda deve ser maior que zero.", nameof(dto.SellPrice));
+
+            if (dto.BuyPrice <= 0)
+                throw new ArgumentException("Preço de compra deve ser maior que zero.", nameof(dto.BuyPrice));
+
+            if (dto.BuyPrice >= dto.SellPrice)
+                throw new ArgumentException("Preço de compra deve ser menor que preço de venda.");
+        }
+    }
+}
