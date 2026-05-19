5de6e5624a30ce07b6fe3eb390d6ad45669bd649
João Vitor Alvarenga <alvarengajv@proton.me>
2026-05-19 00:50:20 -0300

    feat: adicao de servico de monitoramento

---
+++ src/StockQuoteAlert.Application/Services/StockMonitorService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StockQuoteAlert.Domain.Interfaces;
+using StockQuoteAlert.Application.DTOs;
+using StockQuoteAlert.Application.Interfaces;
+
+namespace StockQuoteAlert.Application.Services
+{
+    public class StockMonitorService : IStockMonitorService
+    {
+        private readonly IYahooQuoteService _yahooQuoteService;
+        private readonly IMailKitService _mailKitService;
+
+        public StockMonitorService(IYahooQuoteService yahooQuoteService, IMailKitService mailKitService)
+        {
+            _yahooQuoteService = yahooQuoteService;
+            _mailKitService = mailKitService;
+        }
+
+        public async Task RunAsync(StockAlertDto alert, CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var currentPrice = await _yahooQuoteService.GetStockPriceAsync(alert.Symbol);
+
+                if (currentPrice >= alert.SellPrice)
+                {
+                    await _mailKitService.SendEmailAsync(
+                        $"Alerta de Venda : {alert.Symbol}",
+                        $"O ativo {alert.Symbol} atingiu o preço de venda de {alert.SellPrice:C}. Preço atual: {currentPrice:C}.");
+                }
+                else if (currentPrice <= alert.BuyPrice)
+                {
+                    await _mailKitService.SendEmailAsync(
+                        $"Alerta de Compra: {alert.Symbol}",
+                        $"O ativo {alert.Symbol} atingiu o preço de compra de {alert.BuyPrice:C}. Preço atual: {currentPrice:C}.");
+                }
+
+                await Task.Delay(TimeSpan.FromMinutes(1), cancellationToken);
+            }
+        }
+    }
+}
