044105878c5abd7ccfd791f26713db566ff64eb1
João Vitor Alvarenga <84135038+alvarengajv@users.noreply.github.com>
2026-05-18 22:53:07 -0300

    sqa 1 (#1)
    * feat: adicao da entidade stockalert
    
    * feat: adicao de interface de servicos de infra
    
    * fix: troca de internal para public e remocao de class1

--- src/StockQuoteAlert.Domain/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace StockQuoteAlert.Domain;
-
-public class Class1
-{
-
-}
---
+++ src/StockQuoteAlert.Domain/Entities/StockAlert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockQuoteAlert.Domain.Entities
+{
+    public class StockAlert
+    {
+        public string Symbol { get; set; }
+        public decimal SellPrice { get; set; }
+        public decimal BuyPrice { get; set; }
+
+    }
+}
