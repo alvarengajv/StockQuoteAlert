a8afff677c291a8a61078887e7c0d8ff7f907944
João Alvarenga <84135038+alvarengajv@users.noreply.github.com>
2026-05-19 17:43:12 -0300

    feat: Implementacao da camada infrastructure (#3)
    * feat: adicao do servico externo de monitoramento do Yahoo
    
    * feat: adicao do servico externo mailkit

---
+++ src/StockQuoteAlert.Infrastructure/Services/YahooQuoteService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using StockQuoteAlert.Domain.Interfaces;
+
+namespace StockQuoteAlert.Infrastructure.Services
+{
+    public class YahooQuoteService : IYahooQuoteService
+    {
+        private static readonly HttpClient _http = new HttpClient();
+
+        public async Task<decimal> GetStockPriceAsync(string symbol)
+        {
+            var url = $"https://query1.finance.yahoo.com/v7/finance/quote?symbols={symbol}.SA";
+            var json = await _http.GetStringAsync(url);
+
+            using var doc = JsonDocument.Parse(json);
+            var currentPrice = doc.RootElement
+                .GetProperty("quoteResponse")
+                .GetProperty("result")[0]
+                .GetProperty("regularMarketPrice")
+                .GetDecimal();
+
+            return currentPrice;
+        }
+    }
+}
